ae53c2a0c2ef001b6a91822040f3fd4b62edab10
Michael Liu <michael.liu95@gmail.com>
2014-10-07 18:52:55 -0400

    depth algorithm

--- js/main.js
+++ js/main.js
@@ -15,54 +15,59 @@ var Event = function (start, end) {
 };
 
 function EventTree () {
-  this.tree = [];
+  this.nodes = [];
 };
 
 EventTree.prototype.initialize = function(events, target) {
+  //Remove all children of the target calendar so we can render from scratch
   target.children().remove();
 
   //Sort array by starting time
   var array = events.sort(function (a, b) {
-    return ((a.start < b.start) ? -1 : (a.start > b.start) ? 1 : 0);
+    return ((a.start < b.start) ? 1 : (a.start > b.start) ? -1 : 0);
   });
 
-  array.forEach(function (entry, index) {
-    // var el = document.createElement("div");
-    // el.className = "entry";
-    // el.innerHTML = entry.start;
-    // el.style.marginTop = entry.start + "px";
-    // el.style.height = (entry.end - entry.start) + "px";
-    this.append(entry);
-    // if (!minWidth[index] || minWidth[index] > 600 / overlap) {
-    //   minWidth[index] = 600 / overlap;
-    // }
-    // el.style.width = minWidth[index] + "px";
-    // cal.appendChild(el);
-  }, this);
+  //Until all nodes are placed, find all nodes that don't collide
+  //with each other and place them on the same depth level
+  while (array.length > 0) {
+    //Create a new depth level
+    var currentDepthNodes = [];
+    //By counting backwards, splicing the array and removing an element
+    //will not skip over the next element
+    for (var index = array.length-1; index >= 0; index--) {
+      //Check if this node overlaps with other nodes on the current depth level
+      if(this.noOverlappingNodes(array[index], currentDepthNodes)){
+        currentDepthNodes.push(array.splice(index, 1)[0]);
+      }
+    }
+    this.nodes.push(currentDepthNodes);
+  }
 
+  //Map out the relationships between nodes on different depths
+  this.mapNodes();
+
+  console.log(this.nodes);
   this.render(target);
 }
 
-EventTree.prototype.append = function(entry) {
-  var currentEvent = new Event(entry.start, entry.end)
-  var overlappedEvents = this.tree.filter(function (otherEntry) {
-    //If otherEntry overlaps with current event at any time
-    if ((entry.start >= otherEntry.start && entry.start <= otherEntry.end) || (otherEntry.start >= entry.start && otherEntry.start <= entry.end)) {
-      //Append the current event to the other entry
-      currentEvent.depth = otherEntry.depth + 1;
-      otherEntry.children.push(currentEvent);
-      debugger;
-      return true;
-    }
-  }, this);
+//Returns true if entry collides with any other nodes on currentDepthNodes,
+//otherwise false
+EventTree.prototype.noOverlappingNodes = function(entry, currentDepthNodes) {
+  var currentEvent = new Event(entry.start, entry.end);
+  //Count all other nodes on current depth level that entry collides in time with
+  var overlappedEvents = currentDepthNodes.filter(function (otherEntry) {
+    return (entry.start >= otherEntry.start && entry.start <= otherEntry.end) || (otherEntry.start >= entry.start && otherEntry.start <= entry.end);
+  });
 
-  if (overlappedEvents.length == 0) {
-    this.tree.push(currentEvent);
-  }
+  return overlappedEvents.length == 0;
+}
+
+EventTree.prototype.mapNodes = function() {
+
 }
 
 EventTree.prototype.render = function(target) {
-  this.tree.forEach(function (entry) {
+  this.nodes.forEach(function (entry) {
     var el = document.createElement('div');
     $(el).attr('class', 'entry').css({
       top: entry.start + 'px',
@@ -76,13 +81,6 @@ EventTree.prototype.render = function(target) {
 }
 
 EventTree.prototype.findMaxDepth = function(events) {
-  // if (!events) {
-  //   return depth - 1;
-  // } else {
-  //   events.forEach(function (entry) {
-  //     return 
-  //   });
-  // }
   var depth = 0;
   events.forEach(function (entry) {
     depth = Math.max(depth, this.findMaxDepth(entry.children));
@@ -90,6 +88,7 @@ EventTree.prototype.findMaxDepth = function(events) {
   return depth + 1;
 }
 
+//Render the calendar
 function layOutDay(events) {
   var eventTree = new EventTree();
   eventTree.initialize(events, $("#calendar"));
