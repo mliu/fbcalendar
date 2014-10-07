8086733f7c4472ff0096b7c2bdd3bec47724f60d
Michael Liu <michael.liu95@gmail.com>
2014-10-07 00:04:32 -0400

    new approach

--- js/main.js
+++ js/main.js
@@ -1,28 +1,80 @@
 var sample = [  {start: 30, end:  150}, {start: 540,  end:  600}, {start: 560,  end:  620}, {start: 610,  end: 670}  ];
 
-function layOutDay(events) {
-  var cal = document.getElementById("calendar");
-  var minWidth = [];
+var Event = function (start, end) {
+  this.start = start;
+  this.end = end;
+  this.width = -1;
+  this.height = end - start;
+  this.marginLeft = -1;
+  this.rendered = false;
+  this.title = "Sample Title";
+  this.description = "Lorem Ipsum";
+};
 
-  while (cal.firstChild) {
-    cal.removeChild(cal.firstChild);
-  }
+function EventTree () {
+  this.tree = [];
+};
 
-  events.forEach(function (entry, index) {
-    var el = document.createElement("div");
-    el.className = "entry";
-    el.innerHTML = entry.start;
-    el.style.marginTop = entry.start + "px";
-    el.style.height = (entry.end - entry.start) + "px";
-    var overlap = events.filter(function (entry2) {
-      return (entry.start >= entry2.start && entry.start <= entry2.end) || (entry2.start >= entry.start && entry2.start <= entry.end);
-    }).length;
-    if (!minWidth[index] || minWidth[index] > 600 / overlap) {
-      minWidth[index] = 600 / overlap;
+EventTree.prototype.initialize = function(events, target) {
+  target.children().remove();
+
+  //Sort array by starting time
+  var array = events.sort(function (a, b) {
+    return ((a.start < b.start) ? -1 : (a.start > b.start) ? 1 : 0);
+  });
+
+  array.forEach(function (entry, index) {
+    // var el = document.createElement("div");
+    // el.className = "entry";
+    // el.innerHTML = entry.start;
+    // el.style.marginTop = entry.start + "px";
+    // el.style.height = (entry.end - entry.start) + "px";
+    this.append(entry);
+    // if (!minWidth[index] || minWidth[index] > 600 / overlap) {
+    //   minWidth[index] = 600 / overlap;
+    // }
+    // el.style.width = minWidth[index] + "px";
+    // cal.appendChild(el);
+  }, this);
+
+  this.render(target);
+}
+
+EventTree.prototype.append = function(entry) {
+  var overlappedEvents = this.tree.filter(function (otherEntry) {
+    //If otherEntry overlaps with current event at any time
+    if ((entry.start >= otherEntry.start && entry.start <= otherEntry.end) || (otherEntry.start >= entry.start && otherEntry.start <= entry.end)) {
+      this.tree.push(otherEntry);
+      return true;
+    } else {
+      return false;
     }
-    el.style.width = minWidth[index] + "px";
-    cal.appendChild(el);
+  }, this);
+
+  if (overlappedEvents.length == 0) {
+    this.tree.push(new Event(entry.start, entry.end));
+  }
+}
+
+EventTree.prototype.render = function(target) {
+  this.tree.forEach(function (entry) {
+    var el = document.createElement('div');
+    $(el).attr('class', 'entry').css({
+      top: entry.start + 'px',
+      width: '600px',
+      height: entry.height,
+    });
+    el.innerHTML = '<span><strong>' + entry.title + '</strong></span>' +
+                   '<br><span>' + entry.description + '</span>';
+    target.append(el);
   });
 }
 
-layOutDay(sample);
+function layOutDay(events) {
+  var eventTree = new EventTree();
+  eventTree.initialize(events, $("#calendar"));
+}
+
+$(document).ready(function () {
+  layOutDay(sample);
+});
