4e762ddcbc518646fbf5d785ab077c8ecec2cec3
Michael Liu <michael.liu95@gmail.com>
2014-10-08 14:43:32 -0400

    finished algorithm

--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
-var sample = [  {start: 30, end:  150}, {start: 540,  end:  600}, {start: 560,  end:  620}, {start: 610,  end: 670}  ];
+var sample = [ {start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670} ];
 
+//Node element created for each event used in EventTree
 var Event = function (start, end) {
   this.start = start;
   this.end = end;
-  this.width = -1;
   this.height = end - start;
-  this.marginLeft = -1;
-  this.rendered = false;
   this.title = "Sample Title";
   this.description = "Lorem Ipsum";
-  this.maxDepth = 0;
-  this.depth = 0;
   this.children = [];
 };
 
-function EventTree () {
+//Creates a tree from an array of JS objects and renders it
+function EventTree() {
   this.nodes = [];
-};
+}
 
-EventTree.prototype.initialize = function(events, target) {
+//Takes array of events and target element to render calendar on.
+//Algorithm for generating calendar is as follows:
+/*
+    1.Assign all elements that don't overlap to a single depth level, then
+      start on a new depth level until all elements are placed.
+    2.Go through each depth level and the one directly following it. If any node
+      overlaps with one deeper than it, add the deeper one to the node's list of children.
+    3.We can now recursively search through each node and its children to determine how
+      deep a node stretches, which lets us calculate its width and position.
+*/
+EventTree.prototype.initialize = function (events, target) {
+  var currentDepthNodes, index, temp;
   //Remove all children of the target calendar so we can render from scratch
   target.children().remove();
 
@@ -31,64 +39,92 @@ EventTree.prototype.initialize = function(events, target) {
   //with each other and place them on the same depth level
   while (array.length > 0) {
     //Create a new depth level
-    var currentDepthNodes = [];
+    currentDepthNodes = [];
     //By counting backwards, splicing the array and removing an element
-    //will not skip over the next element
-    for (var index = array.length-1; index >= 0; index--) {
-      //Check if this node overlaps with other nodes on the current depth level
-      if(this.noOverlappingNodes(array[index], currentDepthNodes)){
-        currentDepthNodes.push(array.splice(index, 1)[0]);
+    //will not skip over the next element in the iteration
+    for (index = array.length - 1; index >= 0; index--) {
+      //If this node doesn't overlap with other nodes on the current depth level
+      //add it to the current depth level array
+      if (this.noOverlappingNodes(array[index], currentDepthNodes)) {
+        temp = array.splice(index, 1)[0];
+        currentDepthNodes.push(new Event(temp.start, temp.end));
       }
     }
     this.nodes.push(currentDepthNodes);
   }
 
-  //Map out the relationships between nodes on different depths
+  //Map out the children relationships
   this.mapNodes();
 
-  console.log(this.nodes);
+  //Creates the proper HTML elements and renders them on target element
   this.render(target);
-}
+};
 
 //Returns true if entry collides with any other nodes on currentDepthNodes,
 //otherwise false
-EventTree.prototype.noOverlappingNodes = function(entry, currentDepthNodes) {
-  var currentEvent = new Event(entry.start, entry.end);
+EventTree.prototype.noOverlappingNodes = function (entry, currentDepthNodes) {
   //Count all other nodes on current depth level that entry collides in time with
   var overlappedEvents = currentDepthNodes.filter(function (otherEntry) {
-    return (entry.start >= otherEntry.start && entry.start <= otherEntry.end) || (otherEntry.start >= entry.start && otherEntry.start <= entry.end);
-  });
-
-  return overlappedEvents.length == 0;
-}
-
-EventTree.prototype.mapNodes = function() {
-
-}
-
-EventTree.prototype.render = function(target) {
-  this.nodes.forEach(function (entry) {
-    var el = document.createElement('div');
-    $(el).attr('class', 'entry').css({
-      top: entry.start + 'px',
-      width: (600 / this.findMaxDepth(entry.children, entry.depth)) + 'px',
-      height: entry.height,
-    });
-    el.innerHTML = '<span><strong>' + entry.title + '</strong></span>' +
-                   '<br><span>' + entry.description + '</span>';
-    target.append(el);
+    return this.checkOverlap(entry, otherEntry);
   }, this);
-}
 
-EventTree.prototype.findMaxDepth = function(events) {
+  return overlappedEvents.length === 0;
+};
+
+//Looks through each depth level and the one directly after it
+//populates the children array of each Event object
+EventTree.prototype.mapNodes = function () {
+  var i, j, k, node1, node2;
+
+  for (i = 0; i < this.nodes.length - 1; i++) {
+    for (j = 0; j < this.nodes[i].length; j++) {
+      for (k = 0; k < this.nodes[i + 1].length; k++) {
+        node1 = this.nodes[i][j];
+        node2 = this.nodes[i + 1][k];
+        if (this.checkOverlap(node1, node2)) {
+          node1.children.push(node2);
+        }
+      }
+    }
+  }
+};
+
+//Returns true if node1 overlaps node2, otherwise false
+EventTree.prototype.checkOverlap = function (node1, node2) {
+  return (node1.start >= node2.start && node1.start <= node2.end) || (node2.start >= node1.start && node2.start <= node1.end);
+};
+
+//Goes through each node in the EventTree and calculates width and position,
+//then renders on target element
+EventTree.prototype.render = function (target) {
+  var i, j, el;
+
+  for (i = 0; i < this.nodes.length; i++) {
+    for (j = 0; j < this.nodes[i].length; j++) {
+      el = document.createElement('div');
+      $(el).attr('class', 'entry').css({
+        marginTop: this.nodes[i][j].start + 'px',
+        width: (600 / (i + this.findMaxDepth(this.nodes[i][j].children))) + 'px',
+        marginLeft: i * (600 / (i + this.findMaxDepth(this.nodes[i][j].children))) + 'px',
+        height: this.nodes[i][j].height,
+      });
+      el.innerHTML = '<span><strong>' + this.nodes[i][j].title + '</strong></span>' +
+                     '<br><span>' + this.nodes[i][j].description + '</span>';
+      target.append(el);
+    }
+  }
+};
+
+//Recursive search for maximum depth of a node and its children
+EventTree.prototype.findMaxDepth = function (events) {
   var depth = 0;
   events.forEach(function (entry) {
     depth = Math.max(depth, this.findMaxDepth(entry.children));
   }, this);
   return depth + 1;
-}
+};
 
-//Render the calendar
+//Global function that creates a new EventTree on the #calendar element
 function layOutDay(events) {
   var eventTree = new EventTree();
   eventTree.initialize(events, $("#calendar"));
